466f6de533f1c9674871cc880574072257d90eef
Andreas Ek <andreas.ek@leasify.se>
2023-08-07 16:38:32 +0200

    No action

--- src/Providers/SubscriptionsServiceProvider.php
+++ src/Providers/SubscriptionsServiceProvider.php
@@ -58,8 +58,8 @@ class SubscriptionsServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Publish Resources
-        $this->publishesConfig('rinvex/laravel-subscriptions');
-        $this->publishesMigrations('rinvex/laravel-subscriptions');
-        ! $this->autoloadMigrations('rinvex/laravel-subscriptions') || $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        // $this->publishesConfig('rinvex/laravel-subscriptions');
+        // $this->publishesMigrations('rinvex/laravel-subscriptions');
+        // ! $this->autoloadMigrations('rinvex/laravel-subscriptions') || $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
     }
 }
